83e599be513e67e930f05d1af3c7feaa59bf1cd4
srohimah <sitirohimach@gmail.com>
2018-02-13 19:50:27 +0700

    cookies and ovens

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,10 +1,55 @@
-// Answer These Questions:
-//
-// - What are essential classes?
-// - What attributes will each class have?
-// - What interface will each class provide?
-// - How will the classes interact with each other?
-// - Which classes will inherit from others, if any?
-//
-//
-// Your code here
+class Cookie{
+    constructor(){
+        this.name = 'Cookie'
+        this.status = 'mentah'
+        this.cookingTime = 0
+    }
+    baking(time){
+        for(let i = 5; i<=time; i+=5){
+            if(i < this.cookingTime-10){
+                this.status = 'mentah'
+            }else if(i === this.cookingTime-5){
+                this.status = 'hampir matang'
+            }else if(i === this.cookingTime){
+                this.status = 'matang'
+            }else if(i > this.cookingTime){
+                this.status = 'hangus'
+            }
+            console.log(`${this.name}, menit ke ${i} : ${this.status}`)
+        }
+    }
+
+}
+
+class Chocolate extends Cookie{
+    constructor(){
+        super()
+        this.name = 'Kue Coklat'
+        this.cookingTime = 20
+    }
+}
+
+class Cheese extends Cookie{
+    constructor(){
+        super()
+        this.name = 'Kue Keju'
+        this.cookingTime = 35
+    }
+}
+
+class Peanut extends Cookie{
+    constructor(){
+        super()
+        this.name = 'Kue Kacang'
+        this.cookingTime = 30
+    }
+}
+
+let chocolate = new Chocolate()
+chocolate.baking(25)
+
+let cheese = new Cheese()
+cheese.baking(40)
+
+let peanut = new Peanut()
+peanut.baking(30)
