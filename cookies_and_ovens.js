e5f5b20e81f91ccb9cf68b0bed7b5a8f0dbf9c57
Herby Herado <herby,herado@gmail.com>
2018-02-13 20:30:43 +0700

    Release 2

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,55 @@
 //
 //
 // Your code here
+
+class Cookie {
+    constructor(name){
+        this._name = name || 'Kue'
+        this._time = 0
+        this._bakeTime = 40
+        this._status = this.status()
+    }
+    addTime(){
+        this._time += 5
+    }
+    status (){
+        if (this._time < this._bakeTime-10){
+            console.log(`${this._name}, menit ke-${this._time} : mentah`)
+        } else if (this._time < this._bakeTime-5){
+            console.log(`${this._name}, menit ke-${this._time} : hampir matang`)
+        } else if (this._time < this._bakeTime){
+            console.log(`${this._name}, menit ke-${this._time} : matang`)
+        } else {
+            console.log(`${this._name}, menit ke-${this._time} : hangus`)
+        }
+    }
+}
+
+class PeanutCookie extends Cookie {
+    constructor(){
+        super('Kue Kacang')
+    }
+}
+
+class ChocolateCookie extends Cookie {
+    constructor(){
+        super('Kue Coklat')
+        this._bakeTime = 30
+    }
+}
+
+class CheeseCookie extends Cookie {
+    constructor(){
+        super('Kue Keju')
+        this._bakeTime = 35
+    }   
+}
+
+let peanut = new PeanutCookie()
+let chocolate = new ChocolateCookie()
+let cheese = new CheeseCookie()
+
+for (let i = 0; i < 10; i++){
+    kacang.addTime()
+    kacang.status()
+}
