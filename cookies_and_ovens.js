c5b588207ec5e67cd10ec7c835089a83f0eb792a
adhiartaputra <adhiartaprabataputra@gmail.com>
2018-02-13 17:07:23 +0700

    Release 0

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,3 +1,4 @@
+'use strict'
 // Answer These Questions:
 //
 // - What are essential classes?
@@ -8,3 +9,49 @@
 //
 //
 // Your code here
+class Kue {
+  constructor(name){
+    this.name = name
+    this.bakeTime = null
+    this.status = null
+  }
+
+  bake(input) {
+    for (let i = 5; i <= input; i+=5) {
+      this.status = 'mentah'
+      if(this.bakeTime - 5 === i) {
+        this.status = 'hampir matang'
+      } else if(this.bakeTime === i) {
+        this.status = 'matang'
+      } else if(i > this.bakeTime) {
+        this.status = 'gosong'
+      }
+      console.log(`Kue ${this.name}, menit ke ${i} : ${this.status}`);
+    }
+  }
+}
+
+class KueCokelat extends Kue {
+  constructor () {
+    super()
+    this.bakeTime = 20
+  }
+}
+
+class KueKacang extends Kue {
+  constructor () {
+    super()
+    this.bakeTime = 30
+  }
+}
+
+class KueKeju extends Kue {
+  constructor () {
+    super()
+    this.bakeTime = 35
+  }
+}
+
+let choco = new KueCokelat('Choco')
+
+choco.bake(25)
