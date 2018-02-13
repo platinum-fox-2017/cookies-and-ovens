85f50bfaedd6e09e917ef1cf9dd3b58edeba1cad
Ardhiansyah Putra <ardhimetanaputra@gmail.com>
2018-02-13 15:53:20 +0700

    Release 2: Refactor

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,10 +1,46 @@
-// Answer These Questions:
-//
-// - What are essential classes?
-// - What attributes will each class have?
-// - What interface will each class provide?
-// - How will the classes interact with each other?
-// - Which classes will inherit from others, if any?
-//
-//
-// Your code here
+class Cake {
+	constructor(name, time_cooked) {
+		this._name = name;
+		this._time = 0;
+		this._time_cooked = time_cooked;
+		this._status = 'mentah';
+	}
+
+	cook() {
+		this._time += 5;
+		if (this._time_cooked - 5 == this._time) this._status = 'hampir matang';
+		else if (this._time_cooked == this._time) this._status = 'matang';
+		else if (this._time_cooked < this._time) this._status = 'hangus';
+
+		console.log(`${this._name}, menit ke ${this._time} : ${this._status}`);
+	}
+}
+
+class Chocolate extends Cake {
+	constructor(name, time_cooked) {
+		super(name, time_cooked);
+	}
+}
+
+class Peanut extends Cake {
+	constructor(name, time_cooked) {
+		super(name, time_cooked);
+	}
+}
+
+class Cheese extends Cake {
+	constructor(name, time_cooked) {
+		super(name, time_cooked);
+	}
+}
+
+let kue_coklat = new Chocolate('Kue coklat', 20);
+let kue_kacang = new Chocolate('Kue kacang', 30);
+let kue_keju = new Chocolate('Kue keju', 35);
+let time_cooked = 25;
+
+for (var i = 0; i < time_cooked; i+=5) {
+	kue_coklat.cook();
+	kue_kacang.cook();
+	kue_keju.cook();
+}
