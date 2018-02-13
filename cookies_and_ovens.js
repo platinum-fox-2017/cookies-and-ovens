5983a07c99f5f5a21c6913b18669fcfedfc648b4
Ozi <ozi@Ozis-MacBook-Pro.local>
2018-02-13 20:14:42 +0700

    release 0 done

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,33 @@
 //
 //
 // Your code here
+class Kue {
+    constructor(object){
+        this._time_to_bake = 20;
+    }
+}
+class KueCoklat extends Kue {
+    constructor(object){
+        super(object)
+    }
+}
+class KueKacang extends Kue {
+    constructor(object){
+        super(object)
+        this._time_to_bake = 30;
+    }
+}
+class KueKeju extends Kue {
+    constructor(object){
+        super(object)
+        this._time_to_bake = 35;
+    }
+}
+
+let kue_lapis_coklat = new KueCoklat;
+let kue_lapis_kacang = new KueKacang;
+let kue_lapis_keju = new KueKeju;
+
+console.log(kue_lapis_coklat);
+console.log(kue_lapis_kacang);
+console.log(kue_lapis_keju);
