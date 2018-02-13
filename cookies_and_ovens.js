ee9c8f4d90214163b30397d2e6396bcf559806f3
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-13 13:45:17 +0700

    Release 0: Design The Structure

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,6 +1,7 @@
 // Answer These Questions:
 //
 // - What are essential classes?
+// Cookie , Oven 
 // - What attributes will each class have?
 // - What interface will each class provide?
 // - How will the classes interact with each other?
@@ -8,4 +9,30 @@
 //
 //
 // Your code here
+/*
+Ketentuan 
+Kue coklat 20 menit
+Kue kacang 30 menit
+Kue keju 35 menit
+*/
+
+
+class Oven {
+  constructor(kue,lama_panggang) {
+    this.kue = kue;
+    this.lama_panggang = lama_panggang;
+  }
+  panggang(){
+    
+  }
+}
+
+class Kue {
+  constructor(name,waktu_matang)  {
+    this.name = name;
+    this.waktu_matang = 0;
+    this.status = 'mentah';
+  }
+
+}
 
