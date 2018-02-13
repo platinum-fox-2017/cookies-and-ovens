75ac0a7bf337a767e432f6250ceb677d29e42181
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-13 14:04:50 +0700

    Release 1: Add additional Feature

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -23,16 +23,40 @@ class Oven {
     this.lama_panggang = lama_panggang;
   }
   panggang(){
-    
+    if(this.lama_panggang % 5 != 0 ){
+      console.log('Waktu Panggang Harus Kelipatan 5');
+    } else if(this.lama_panggang < 5 ){
+      console.log('Waktu Panggang Tidak Boleh Kurang dari 5')  ;
+    } else {
+      let kaliPanggang = this.lama_panggang / 5;
+      let waktuSetengahMatang = Math.round(this.kue.waktu_matang / 2);
+      let waktuMatang = this.kue.waktu_matang;
+      let menitKe = 5;
+      for(let i = 0; i < kaliPanggang; i++){
+        if(menitKe < waktuSetengahMatang){
+          var status_kue = 'mentah';
+        } else if(menitKe === waktuMatang ){
+          var status_kue = 'matang';
+        } else if(menitKe > waktuSetengahMatang && menitKe < waktuMatang){
+          var status_kue = 'hampir matang';
+        } else if(menitKe > waktuMatang ){
+          var status_kue = 'hangus';
+        }
+         console.log(`${this.kue.name}, menit ke ${menitKe} : ${status_kue} `) ;
+         menitKe += 5;
+      }
+    }
   }
 }
 
 class Kue {
   constructor(name,waktu_matang)  {
     this.name = name;
-    this.waktu_matang = 0;
+    this.waktu_matang = waktu_matang;
     this.status = 'mentah';
   }
-
 }
+let kue = new Kue('Kue Cokelat',20);
+let oven = new Oven(kue,30);
+oven.panggang()
 
