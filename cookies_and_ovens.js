e2d7125d338aebc9c67661c0090d5e199b598188
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-13 13:15:48 +0700

    Release 1: Add requirement to Inheritance

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -10,9 +10,9 @@
 // Your code here
 
 class Cookie {
-  constructor()  {
+  constructor(ingredients)  {
     this.status = 'mentah';
-    this.ingredients = [];
+    this.ingredients = ingredients;
   }
   bake(){
     this.status = "selesai dimasak";
@@ -20,22 +20,37 @@ class Cookie {
 }
 
 class PeanutButter extends Cookie {
-  constructor(){
-    super();
+  constructor(ingredients){
+    super(ingredients);
     this.name = 'peanut butter';
     this.peanut_conut = 100;  
   }
 }
+class PeanutButterCrumbled extends Cookie {
+  constructor(ingredients){
+    super(ingredients);
+    this.name = 'peanut butter crumbled';
+    this.peanut_conut = 100;  
+  }
+}
 class ChocholateChip extends Cookie {
-  constructor(){
-    super();
+  constructor(ingredients){
+    super(ingredients);
     this.name = 'chocolate chip';
     this.choc_chip_count = 200;  
   }
 }
+class ChocholateChipCrumbled extends Cookie {
+  constructor(ingredients){
+    super(ingredients);
+    this.name = 'chocolate chip crumbled';
+    this.choc_chip_count = 200;  
+  }
+}
+
 class OtherCookie extends Cookie {
-  constructor(name){
-    super();
+  constructor(name,ingredients){
+    super(ingredients);
     this.name = name;
     this.other_count = 150;  
   }
