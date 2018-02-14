7a39620a4812730d881767010e461bc44bd50af8
root <irsyadpahlapi28@gmail.com>
2018-02-13 20:54:39 -0500

    update cookies

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,51 +1,47 @@
 class Cookie {
-     constructor(name){
+     constructor(name,waktu){
          this._name = name
          this._time = 0
-         this._maximum = 25
+         this._maximum = waktu || 25
          this._status = this.status()
      }
      addTime(){
          this._time += 5
      }
      status (){
-         if (this._time < this._maximum-10 && this._time !=0){
+       while(this._time<=this._maximum){
+         if (this._time <= this._maximum-10){
              console.log(this._name+', menit ke-'+this._time+' : mentah')
-         } else if (this._time < this._maximum-5 && this._time !=0){
+         } else if (this._time <= this._maximum-5 ){
              console.log(this._name+', menit ke-'+this._time+' : hampir matang')
-         } else if (this._time < this._maximum && this._time !=0){
+         } else if (this._time <= this._maximum){
              console.log(this._name+', menit ke-'+this._time+' : matang')
-         } else if(this._time >= this._maximum){
+         } else if(this._time > this._maximum){
              console.log(this._name+', menit ke-'+this._time+' : hangus')
          }
+         this.addTime()
+       }
      }
  }
 
  class Kacang extends Cookie {
      constructor(){
-         super('Kue Kacang')
-         this._maximum=30
+        super('Kue Kacang',20)
      }
  }
 
  class Coklat extends Cookie {
      constructor(){
-         super('Kue Coklat')
+        super('Kue Coklat')
      }
  }
 
  class Keju extends Cookie {
      constructor(){
-         super('Kue Keju')
+        super('Kue Keju')
      }
  }
 
  let kacang = new Kacang()
  let coklat = new Coklat()
  let keju = new Keju()
-
-
- for (let i = 0; i < 5; i++){
-     kacang.addTime()
-     kacang.status()
- }
